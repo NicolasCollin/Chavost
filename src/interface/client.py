dd4757b1342edebf7709363e7134f38d76dbf7d0
matthisarvois <arvois.matthis60@gmail.com>
2025-12-12 17:33:21 +0100

    modification pour resoudre beugs

--- src/interface/client.py
+++ src/interface/client.py
@@ -7,5 +7,6 @@ def client_tool():
     st.markdown(
         """
 Bienvenue sur **L'explorateur de clients** de Chavost.
+**Dans cette partie** vous allez pouvoir extraire **toutes** les informations possible relatives aux clients qui ont acheté.
 """
     )
